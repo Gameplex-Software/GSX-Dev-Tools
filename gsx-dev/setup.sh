e02c98e1083358aa58e5ee6701824c23d30646b9
Tristan Poland <34868944+tristanpoland@users.noreply.github.com>
2022-01-07 12:57:05 -0500

    Create setup.sh

---
+++ gsx-dev/setup.sh
@@ -0,0 +1,12 @@
+echo "setting up your GXS-Dev environment"
+
+brew install git
+brew install zenity
+
+echo ""
+echo "+------------------------------Installation Wizard-----------------------------------+"
+echo "| Your GXS Developer environment has been set up sucessfully, run 'gxsdev help for   |"
+echo "| more info                                                                          |"
+echo "+------------------------------------------------------------------------------------+"
+echo ""
+
